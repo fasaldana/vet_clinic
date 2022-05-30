69cf5aae95204af311b2e41ccabb7df9a87b685b
fasaldana <fernando_s_98@hotmail.com>
2022-05-30 12:29:52 -0500

    database basic setup

--- queries.sql
+++ queries.sql
@@ -1,3 +1,9 @@
 /*Queries that provide answers to the questions from all projects.*/
-
-SELECT * from animals WHERE name = 'Luna';
+SELECT name FROM vet_clinic.animals where name like '%mon';
+SELECT name FROM vet_clinic.animals where date_of_birth between '2016-01-01' and '2019-12-31';
+SELECT name FROM vet_clinic.animals where neutered = 1 and escape_attempts < 3;
+SELECT date_of_birth FROM vet_clinic.animals where name = 'Agumon' or name = 'Pikachu';
+SELECT name, escape_attempts FROM vet_clinic.animals where weight_kg > 10.5;
+SELECT * FROM vet_clinic.animals where neutered = 1;
+SELECT * FROM vet_clinic.animals where name != 'Gabumon';
+SELECT * FROM vet_clinic.animals where weight_kg between 10.4 and 17.3;
--- schema.sql
+++ schema.sql
@@ -1,5 +1,10 @@
 /* Database schema to keep the structure of entire database. */
-
-CREATE TABLE animals (
-    name varchar(100)
-);
+CREATE SCHEMA `vet_clinic` ;
+CREATE TABLE `vet_clinic`.`animals` (
+  `idAnimals` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL,
+  `date_of_birth` DATETIME NOT NULL,
+  `escape_attempts` INT NOT NULL,
+  `neutered` TINYINT(1) NOT NULL,
+  `weight_kg` DECIMAL(3) NOT NULL,
+  PRIMARY KEY (`idAnimals`));
