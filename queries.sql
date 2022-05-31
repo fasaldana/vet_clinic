4c02faff2bc54563e838f2318a92be510d6677ba
fasaldana <fernando_s_98@hotmail.com>
2022-05-30 23:07:06 -0500

    migrated from mysql to postgresql

--- queries.sql
+++ queries.sql
@@ -1,9 +1,9 @@
 /*Queries that provide answers to the questions from all projects.*/
-SELECT name FROM vet_clinic.animals where name like '%mon';
-SELECT name FROM vet_clinic.animals where date_of_birth between '2016-01-01' and '2019-12-31';
-SELECT name FROM vet_clinic.animals where neutered = 1 and escape_attempts < 3;
-SELECT date_of_birth FROM vet_clinic.animals where name = 'Agumon' or name = 'Pikachu';
-SELECT name, escape_attempts FROM vet_clinic.animals where weight_kg > 10.5;
-SELECT * FROM vet_clinic.animals where neutered = 1;
-SELECT * FROM vet_clinic.animals where name != 'Gabumon';
-SELECT * FROM vet_clinic.animals where weight_kg between 10.4 and 17.3;
+SELECT name FROM animals where name like '%mon';
+SELECT name FROM animals where date_of_birth between '2016-01-01' and '2019-12-31';
+SELECT name FROM animals where neutered = 1 and escape_attempts < 3;
+SELECT date_of_birth FROM animals where name = 'Agumon' or name = 'Pikachu';
+SELECT name, escape_attempts FROM animals where weight_kg > 10.5;
+SELECT * FROM animals where neutered = 1;
+SELECT * FROM animals where name != 'Gabumon';
+SELECT * FROM animals where weight_kg between 10.4 and 17.3;
--- schema.sql
+++ schema.sql
@@ -1,10 +1,12 @@
 /* Database schema to keep the structure of entire database. */
-CREATE SCHEMA `vet_clinic` ;
-CREATE TABLE `vet_clinic`.`animals` (
-  `idAnimals` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(45) NOT NULL,
-  `date_of_birth` DATETIME NOT NULL,
-  `escape_attempts` INT NOT NULL,
-  `neutered` TINYINT(1) NOT NULL,
-  `weight_kg` DECIMAL(3) NOT NULL,
-  PRIMARY KEY (`idAnimals`));
+CREATE DATABASE vetclinic
+CREATE TABLE public.animals
+(
+    id integer NOT NULL,
+    name "char" NOT NULL,
+    date_of_birth date NOT NULL,
+    escape_attempts integer NOT NULL,
+    neutered boolean NOT NULL,
+    weight_kg numeric NOT NULL,
+    CONSTRAINT animals_pkey PRIMARY KEY (id)
+)
