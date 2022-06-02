8347a643fb42e8ce16463621a2bfe67a80b47e50
fasaldana <fernando_s_98@hotmail.com>
2022-06-01 19:49:50 -0500

    Added multiple tables and queries

--- queries.sql
+++ queries.sql
@@ -46,3 +46,56 @@ SELECT species, MIN(weight_kg) as min_weight, MAX(weight_kg) as max_weight FROM 
 
 /* What is the average number of escape attempts per animal type of those born between 1990 and 2000? */
 SELECT species, AVG(escape_attempts) as average_scape_attempts FROM animals WHERE date_of_birth between '1990-01-01' and '2000-12-31' GROUP BY species
+
+/* What animals belong to Melody Pond? */
+SELECT a.name, o.full_name
+FROM animals a
+INNER JOIN owners o
+on a.owner_id=o.id
+WHERE o.full_name = 'Melody Pond';
+
+/* List of all animals that are pokemon (their type is Pokemon). */
+SELECT a.name, s.name
+FROM animals a
+INNER JOIN species s
+ON a.species_id = s.id
+WHERE s.name = 'Pokemon';
+
+/* List all owners and their animals, remember to include those that don't own any animal. */
+SELECT o.full_name, a.name
+FROM owners o
+LEFT JOIN animals a
+on a.owner_id=o.id;
+
+/* How many animals are there per species? */
+SELECT COUNT(a.name) as animals_quantity, s.name
+FROM animals a
+INNER JOIN species s
+ON a.species_id = s.id
+GROUP BY s.name;
+
+/* List all Digimon owned by Jennifer Orwell. */
+SELECT a.name, o.full_name as owner, s.name as species
+FROM animals a
+JOIN owners o
+on a.owner_id = o.id
+JOIN species s
+on a.species_id = s.id
+WHERE o.full_name = 'Jennifer Orwell'
+AND s.name = 'Digimon';
+
+/* List all animals owned by Dean Winchester that haven't tried to escape. */
+SELECT a.name as animal, o.full_name as owner, a.escape_attempts
+FROM animals a
+INNER JOIN owners o
+on a.owner_id=o.id
+WHERE escape_attempts = 0
+AND o.full_name = 'Dean Winchester';
+
+/* Who owns the most animals? */
+SELECT o.full_name as owner, COUNT(a.name) AS quantity
+FROM animals a
+INNER JOIN owners o
+on a.owner_id=o.id
+GROUP BY o.full_name
+ORDER BY quantity DESC;
--- schema.sql
+++ schema.sql
@@ -1,13 +1,38 @@
 /* Database schema to keep the structure of entire database. */
 CREATE DATABASE vetclinic
-CREATE TABLE animals
+CREATE TABLE owners
 (
-    id integer NOT NULL,
-    name varchar(50) NOT NULL,
+    id serial NOT NULL,
+    full_name varchar(50),
+    age integer,
+    CONSTRAINT owners_pkey PRIMARY KEY (id)
+);
+
+CREATE TABLE species
+(
+    id serial NOT NULL,
+    name varchar(50),
+    CONSTRAINT species_pkey PRIMARY KEY (id)
+);
+
+CREATE TABLE animals(
+    id serial NOT NULL,
+    name character varying(50) COLLATE pg_catalog."default" NOT NULL,
     date_of_birth date NOT NULL,
     escape_attempts integer NOT NULL,
     neutered boolean NOT NULL,
     weight_kg numeric NOT NULL,
-    species varchar(50),
-    CONSTRAINT animals_pkey PRIMARY KEY (id)
-)
+    species_id integer,
+    owner_id integer,
+    CONSTRAINT animals_pkey PRIMARY KEY (id),
+    CONSTRAINT owners_id_fkey FOREIGN KEY (owner_id)
+        REFERENCES owners (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+        NOT VALID,
+    CONSTRAINT species_id_fkey FOREIGN KEY (species_id)
+        REFERENCES species (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+        NOT VALID
+);
